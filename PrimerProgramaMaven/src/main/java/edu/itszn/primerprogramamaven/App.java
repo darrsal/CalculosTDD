0c793371353e734a38a0b5074fcc148e4d1e4a6a
Daniel Arredondo Salcedo <daniels@cimat.mx>
2024-11-20 09:05:20 -0600

    Primera version del programa

---
+++ PrimerProgramaMaven/src/main/java/edu/itszn/primerprogramamaven/App.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.itszn.primerprogramamaven;
+
+/**
+ *
+ * @author Darre
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Calculos mCal = new Calculos();
+
+        System.out.println("Suma entre 2 y 4 " + mCal.sumarSerie(2, 4));
+        System.out.println("Absoluto de -3 " + mCal.absoluto(-3));
+        System.out.println("Vocales de 'murcielago' " + mCal.vocales("murcielago"));
+        System.out.println("Invertir de 'hola mundo' " + mCal.invertir("hola mundo"));
+    }
+}
---
+++ PrimerProgramaMaven/src/main/java/edu/itszn/primerprogramamaven/Calculos.java
@@ -0,0 +1,88 @@
+package edu.itszn.primerprogramamaven;
+
+public class Calculos {
+
+    /**
+     * *
+     * Calcula la sumatoria de los numeros entre "x" a "y", incluyendolas
+     *
+     * @param x
+     * @param y
+     * @return sumatoria
+     */
+    int sumarSerie(int x, int y) {
+        if (x > y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        int suma = 0;
+        for (int i = x; i <= y; i++) {
+            suma = suma + i;
+        }
+        return suma;
+    }
+
+    /**
+     * *
+     * Calcula el valor absoluto de un numero
+     *
+     * @param num
+     * @return valor absoluto
+     */
+    float absoluto(float num) {
+        float res = Math.abs(num);
+        return res;
+    }
+
+    /**
+     * *
+     * Cuenta las vocales en una frase
+     *
+     * @param texto
+     * @return cantidad de vocales
+     */
+    int vocales(String texto) {
+        if (texto == null) {
+            return 0;
+        } else {
+            String vocales = "aeiou";
+            char[] items = texto.toLowerCase().toCharArray();
+
+            int cont = 0;
+            for (char letra : items) {
+                if (vocales.indexOf(letra) != -1) {
+                    cont++;
+                }
+            }
+            return cont;
+        }
+    }
+
+    /**
+     * *
+     * Invierte el orden de las letras en cada palabra, pero no altera el orden
+     * de las palabras
+     *
+     * @param texto
+     * @return texto invertida
+     */
+    String invertir(String texto) {
+        if (texto == null) {
+            return null;
+        } else if (texto.length() == 0) {
+            return "";
+        } else {
+            String[] listaPalabras = texto.split(" ");
+            StringBuilder textoInv = new StringBuilder();
+            for (String palabra : listaPalabras) {
+                StringBuilder palabraInv = new StringBuilder();
+                palabraInv.append(palabra);
+                textoInv.append(palabraInv.reverse());
+                textoInv.append(" ");
+            }
+            return textoInv.toString().trim();
+        }
+    }
+
+}
