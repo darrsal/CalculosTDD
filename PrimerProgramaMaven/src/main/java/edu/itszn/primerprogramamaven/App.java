5753b03042958c047c4022088516dfceeb7e5d04
Daniel Arredondo Salcedo <daniels@cimat.mx>
2025-11-06 18:03:42 -0600

    Update App.java

--- PrimerProgramaMaven/src/main/java/edu/itszn/primerprogramamaven/App.java
+++ PrimerProgramaMaven/src/main/java/edu/itszn/primerprogramamaven/App.java
@@ -13,7 +13,6 @@ public class App {
     public static void main(String[] args) {
         Calculos mCal = new Calculos();
         System.out.println("Suma entre 2 y 4 " + mCal.sumarSerie(2, 4));
-        
         System.out.println("Absoluto de -3 " + mCal.absoluto(-3));
         
         System.out.println("Vocales de 'murcielago' " + mCal.vocales("murcielago"));
